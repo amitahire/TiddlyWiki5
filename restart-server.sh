3298725c34aa172b06521fa24224ac02bc68707b
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2017-12-05 21:22:50 +0000

    Fixed skipping of credential check.
    Empty strings have to be passed to the server command for the 'username' and 'password' arguments  in order to open up access to the next positional argument which is the 'host' argument.
    The credentials check is meant to be skipped when empty strings are passed but actually the TiddlyWiki was prompting for a username and password that were both literally double quotes.
    Unescaping the quotes stopped the credential check and produced the desired behaviour of skipping it.

--- restart-server.sh
+++ restart-server.sh
@@ -5,5 +5,5 @@ pidfile=/var/run/tiddlywiki.pid
 
 kill $(< $pidfile)
 cd $tiddlywikidir
-nohup node tiddlywiki.js $wikidir --server 8080 $:/core/save/all text/plain text/html \"\" \"\" 0.0.0.0 &
+nohup node tiddlywiki.js $wikidir --server 8080 $:/core/save/all text/plain text/html "" "" 0.0.0.0 &
 echo $! > $pidfile
